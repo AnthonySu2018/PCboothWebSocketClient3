0d077d93dd00f721522a6393f299e4bbe9735a9d
AnthonySioux <anthonysu91@gmail.com>
2024-01-30 17:33:24 +0800

    2.3 work

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,18 @@ plugins {
 
 group = "org.anthony"
 version = "1.0-SNAPSHOT"
+val ktor_version: String by project
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-websockets:$ktor_version")
+    implementation("org.slf4j:slf4j-api:2.0.0")
+    implementation("org.slf4j:slf4j-simple:2.0.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
@@ -19,3 +25,4 @@ tasks.test {
 kotlin {
     jvmToolchain(11)
 }
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,114 @@
-package org.anthony
+import io.ktor.client.*
+import io.ktor.client.plugins.websocket.*
+import io.ktor.http.*
+import io.ktor.websocket.*
+import io.ktor.utils.io.errors.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
+import java.net.InetAddress
+import java.util.concurrent.TimeUnit
 
 fun main() {
-    println("Hello World!")
+    app()
+
+}
+
+private fun app() {
+    val client = HttpClient {
+        install(WebSockets)
+    }
+    println("Author: Anthony")
+    println("Version: 2.3")
+    println("Date: 2024-01-30")
+    println("Please don't close this windows.")
+
+    pingFailReboot()
+    println("The host can be ping. So starting to connect to the host.")
+    connectToServer(client)
+}
+
+private fun connectToServer(client: HttpClient) {
+    runBlocking {
+        try {
+            client.webSocket(method = HttpMethod.Get, host = "192.168.10.2", port = 8080, path = "/chat") {
+                println("Connected to the host.")
+                while (true) {
+                    val othersMessage = incoming.receive() as? Frame.Text ?: continue
+                    println(othersMessage.readText())
+                    val task = othersMessage.readText()
+                    when (task) {
+                        "reboot" -> {
+                            reboot()
+                        }
+                        "shutdown" -> {
+                            shutdown()
+                        }
+                    }
+                    /*                val myMessage = readlnOrNull()
+                                    if(myMessage != null) {
+                                        send(myMessage)
+                                    }*/
+                }
+            }
+        } catch (_: IOException) {
+            println("IOException happen. Restarting the script.")
+            app()
+        } catch (_: ClosedReceiveChannelException) {
+            println("ClosedReceiveChannelException happen.Restarting the script.")
+            app()
+        }
+    }
+}
+
+private fun pingFailReboot(ipAddress: String = "192.168.10.2",
+                            isPingSuccess: Boolean = false,
+                            falseCounter: Int  = 0) {
+
+    var isPingSuccess = isPingSuccess
+    var falseCounter = falseCounter
+    do {
+        ping(ipAddress)
+        isPingSuccess = ping(ipAddress)
+        println("Ping $ipAddress,and $isPingSuccess")
+        TimeUnit.SECONDS.sleep(4)
+        falseCounter++
+        println("failed to connect to host,the $falseCounter time")
+        if (falseCounter > 10) {
+            reboot()
+        }
+    } while (!isPingSuccess)
+}
+
+
+private fun shutdown() {
+    try {
+        val command = "shutdown.exe -p" // -p 表示关机
+        val process = Runtime.getRuntime().exec(command)
+        process.waitFor()
+    } catch (e: IOException) {
+        e.printStackTrace()
+    } catch (e: InterruptedException) {
+        e.printStackTrace()
+    }
+}
+
+private fun reboot() {
+    try {
+        val command = "shutdown.exe -r -t 0 " // -r 表示重启，-t 0 表示无延迟立即重启
+        val process = Runtime.getRuntime().exec(command)
+        process.waitFor()
+    } catch (e: IOException) {
+        e.printStackTrace()
+    } catch (e: InterruptedException) {
+        e.printStackTrace()
+    }
+}
+
+fun ping(ipAddress: String): Boolean {
+    return try {
+        val inet = InetAddress.getByName(ipAddress)
+        inet.isReachable(1000) // 设置超时时间为1秒
+    } catch (ex: Exception) {
+        false
+    }
 }
